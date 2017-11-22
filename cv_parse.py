455c14c461b969f9c52b180bc439763681c757c7
rg3915 <rg3915@yahoo.com.br>
2017-11-22 20:44:25 -0200

    Tentando extrair a data

--- cv_parse.py
+++ cv_parse.py
@@ -11,18 +11,27 @@ _words = pdf_to_string(file_pdf).split('\n')
 
 # Remove espaços vazios nas extremidades do texto
 words = [word.strip() for word in _words]
-
 output = {}
+
+names = ['Regis da Silva Santos']
 
 
 def is_first_name(word):
-    if word == 'Regis da Silva Santos':
+    if word in names:
         output['first_name'] = word
+        return word
+
+
+def is_date(word):
+    if datetime.strptime(word, '%m/%Y') == '%m/%Y':
+        output['date'] = word
         return word
 
 
 for word in words:
     if is_first_name(word):
         words.remove(is_first_name(word))
+    if is_date(word):
+        words.remove(is_date(word))
 
 print(output)
